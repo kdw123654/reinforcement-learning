41fd1ed1789897c489d13b323749dd235e6bd848
KIM DONG WOOK <108047423+kdw123654@users.noreply.github.com>
2025-12-15 18:09:35 +0900

    Update agent.py

--- def agent/agent.py
+++ def agent/agent.py
@@ -1,3 +1,13 @@
+import numpy as np
+
+class Bandit:
+    def __init__(self, action_size=10):
+        self.probs = np.random.rand(action_size)
+
+    def play(self, action):
+        return 1 if np.random.rand() < self.probs[action] else 0
+
+
 class Agent:
   def __init__(self,epsilon,action_size=10):
     self.epsilon=epsilon
@@ -13,7 +23,7 @@ class Agent:
       return np.random.randint(0,len(self.Qs))
     else:
       return np.argmax(self.Qs)
-
+bandit=Bandit
 agent = Agent(epsilon=0.5)
 
 for _ in range(1000):
@@ -21,3 +31,7 @@ for _ in range(1000):
     reward = bandit.play(action)
     agent.update(action, reward)
 print(agent.Qs)
+
+
+[0.9001996  0.59183673 0.16981132 0.72       0.46938776 0.82608696
+ 0.37931034 0.85185185 0.30952381 0.82666667]
